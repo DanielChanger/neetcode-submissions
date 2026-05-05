e763f718cc19275055dda2f5531c930296b535db
Danylo Miniailo <daniel_changer@yahoo.com>
2026-05-05 21:32:46 +0300

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        var bracketsMapping = Map.of('(', ')', '{', '}', '[', ']');
+        var openBrackets = new ArrayDeque<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char bracket = s.charAt(i);
+            if (bracketsMapping.containsKey(bracket)) {
+                openBrackets.push(bracket);
+            } else {
+                if (openBrackets.isEmpty()) {
+                    return false;
+                }
+                char leftBracket = openBrackets.pop();
+                if (bracketsMapping.get(leftBracket) != bracket) {
+                    return false;
+                }
+            }
+        }
+        return openBrackets.isEmpty();
+    }
+}
