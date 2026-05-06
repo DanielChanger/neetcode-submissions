e97409f5da0f4695bb3356fdcbee3a7c1cf99efe
Danylo Miniailo <daniel_changer@yahoo.com>
2026-05-06 23:51:51 +0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,8 @@
+class Solution {
+    // Streams
+    public boolean hasDuplicate(int[] nums) {
+        return Arrays.stream(nums)
+                     .distinct()
+                     .count() != nums.length;
+    }
+}
