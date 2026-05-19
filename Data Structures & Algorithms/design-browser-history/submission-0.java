ca86ab4ec514e6b3e473670aae5af380f36b706a
Danylo Miniailo <daniel_changer@yahoo.com>
2026-05-19 23:05:59 +0300

    Add: design-browser-history - submission-0

---
+++ Data Structures & Algorithms/design-browser-history/submission-0.java
@@ -0,0 +1,48 @@
+class BrowserHistory {
+    private Page current;
+
+    public BrowserHistory(String homepage) {
+        current = new Page(homepage);
+    }
+    
+    public void visit(String url) {
+        var newPage = new Page(url);
+        current.connect(newPage);
+        current = current.next;
+    }
+    
+    public String back(int steps) {
+        for (int i = 0; i < steps && current.prev != null; i++) {
+            current = current.prev;
+        }
+        return current.url;
+    }
+    
+    public String forward(int steps) {
+        for (int i = 0; i < steps && current.next != null; i++) {
+            current = current.next;
+        }
+        return current.url;
+    }
+
+    class Page {
+        String url;
+        Page prev;
+        Page next;
+        Page(String url) {
+            this.url = url;
+        }
+        void connect(Page page) {
+            this.next = page;
+            page.prev = this;
+        }
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
